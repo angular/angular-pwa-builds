bbdc13825a963315f841d911839178c6ef161c6f
Angular Builds <circleci@angular.io>
2021-06-13 16:35:37 +0000

    b3d708014 build: enable `esModuleInterop` TypeScript option

--- pwa/index.js
+++ pwa/index.js
@@ -6,6 +6,25 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const schematics_1 = require("@angular-devkit/schematics");
 const workspace_1 = require("@schematics/angular/utility/workspace");
@@ -17,7 +36,7 @@ function updateIndexFile(path) {
         if (buffer === null) {
             throw new schematics_1.SchematicsException(`Could not read index file: ${path}`);
         }
-        const rewriter = new (await Promise.resolve().then(() => require('parse5-html-rewriting-stream')))();
+        const rewriter = new (await Promise.resolve().then(() => __importStar(require('parse5-html-rewriting-stream')))).default();
         let needsNoScript = true;
         rewriter.on('startTag', (startTag) => {
             if (startTag.tagName === 'noscript') {
